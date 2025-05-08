b22405dc14d746e980f79c5e4745ca18a086335f
Taewan Kim <paladyn12@naver.com>
2025-05-08 18:20:03 +0900

    OAuth2 Login Failure Handler

---
+++ src/main/java/com/paladyn/template/common/exception/CustomAuthExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.paladyn.template.common.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * OAuth2 Login 실패 Handler
+ */
+@Component
+public class CustomAuthExceptionHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.sendRedirect("/login-failure");
+    }
+}
